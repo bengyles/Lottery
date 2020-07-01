0cc7702978cbce16d0e6d1cf116aa9faaa31dc4e
bengyles <bengyles@hotmail.com>
2020-07-01 15:30:24 +0200

    wrote some tests and changed sha3 to keccak256 in the contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -15,7 +15,7 @@ contract Lottery {
     }
 
     function random() private view returns(uint){
-        return  uint(sha3(block.difficulty, now, players));
+        return  uint(keccak256(block.difficulty, now, players));
     }
 
     function pickWinner() public restricted{
