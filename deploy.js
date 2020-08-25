2451026295570424484008508c0237cd604b7853
bengyles <bengyles@hotmail.com>
2020-08-25 17:04:25 +0200

    Upgraded truffle and improved deploy script

--- deploy.js
+++ deploy.js
@@ -15,11 +15,15 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({data: bytecode})
-      .send({gas: '1000000', from: accounts[0]});
+  try {
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: '0x' + bytecode }) // add 0x bytecode
+        .send({from: accounts[0]}); // remove 'gas'
 
-  console.log('Contract deployed to', result.options.address);
+    console.log(interface);
+    console.log('Contract deployed to', result.options.address);
+  }catch(err){
+    console.log(err);
+  }
 };
-
 deploy();
